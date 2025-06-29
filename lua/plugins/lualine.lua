5d4b7d592dc87bf7a20c246e5f4499102b930959
NicoLuhmann <luhmann.nico@gmail.com>
2025-06-29 20:11:34 +0200

    feat(lualine): customize sections and shorten mode display
    Refactor lualine configuration to:
    - Display only the first letter of the mode in lualine_a.
    - Show more path in filenames.
    - Move encoding, fileformat, and filetype to lualine_y.
    - Remove unused sections for a cleaner statusline.
    
    This improves statusline clarity and usability.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,10 +3,37 @@ return {
 	cond = not vim.g.vscode,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("lualine").setup {
-      options = {
-        theme = "catppuccin"
-      }
-    }
+		require("lualine").setup({
+			options = {
+				theme = "catppuccin",
+			},
+			sections = {
+				lualine_a = {
+					{
+						"mode",
+						fmt = function(str)
+							return str:sub(1, 1)
+						end,
+					},
+				},
+				lualine_c = {
+					{ "filename", path = 4 },
+				},
+				lualine_x = {},
+				lualine_y = {
+					"encoding",
+					"fileformat",
+					"filetype",
+				},
+			},
+			inactive_sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 3,
+					},
+				},
+			},
+		})
 	end,
 }
