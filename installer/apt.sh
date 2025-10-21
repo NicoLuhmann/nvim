e4d189b138961c66a3959ead3bc4754a5b6cbbb5
NicoLuhmann <luhmann.nico@gmail.com>
2025-10-21 03:42:27 +0200

    feat(installer): add node installation

--- installer/apt.sh
+++ installer/apt.sh
@@ -66,6 +66,23 @@ fi
 
 echo ""
 echo "--------------------------------------------------------"
+echo "Installing nvm and Node.js..."
+echo "--------------------------------------------------------"
+
+export NVM_DIR="$HOME/.nvm"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+nvm install 25
+nvm use 25
+
+echo "Node.js $(node --version) installed successfully."
+echo "npm $(npm --version) installed successfully."
+
+echo ""
+echo "--------------------------------------------------------"
 echo "Neovim setup complete!"
 echo "--------------------------------------------------------"
 
