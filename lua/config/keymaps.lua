35749cbfb209df8627f509883a064616ec104258
NicoLuhmann <luhmann.nico@gmail.com>
2025-10-23 02:19:47 +0200

    feat: adjust to mimic tmux and terminal behavior

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -34,11 +34,25 @@ else
     keymap.set("n", "<leader>qo", "<cmd>copen<CR>", { desc = "Open Quickfix" })
     keymap.set("n", "<leader>qc", "<cmd>cclose<CR>")
 
-    keymap.set("n", "+", ":vertical resize +5<CR>")
-    keymap.set("n", "_", ":vertical resize -5<CR>")
-    keymap.set("n", "=", ":resize +5<CR>")
-    keymap.set("n", "-", ":resize -5<CR>")
+    keymap.set("n", "<C-w>>", ":vertical resize +10<CR>")
+    keymap.set("n", "<C-w><", ":vertical resize -10<CR>")
+    keymap.set("n", "<C-w>+", ":resize +10<CR>")
+    keymap.set("n", "<C-w>_", ":resize -10<CR>")
+    keymap.set("n", "<C-w>=", "<C-w>=")
 
-    keymap.set("n", "<Right>", ":bnext<CR>", opts)
-    keymap.set("n", "<Left>", ":bprevious<CR>", opts)
+    keymap.set("n", "<C-w>n", ":bnext<CR>", { desc = "Next buffer" })
+    keymap.set("n", "<C-w>p", ":bprevious<CR>", { desc = "Previous buffer" })
+    
+    keymap.set("n", "<C-w>1", ":buffer 1<CR>", { desc = "Buffer 1" })
+    keymap.set("n", "<C-w>2", ":buffer 2<CR>", { desc = "Buffer 2" })
+    keymap.set("n", "<C-w>3", ":buffer 3<CR>", { desc = "Buffer 3" })
+    keymap.set("n", "<C-w>4", ":buffer 4<CR>", { desc = "Buffer 4" })
+    keymap.set("n", "<C-w>5", ":buffer 5<CR>", { desc = "Buffer 5" })
+    keymap.set("n", "<C-w>6", ":buffer 6<CR>", { desc = "Buffer 6" })
+    keymap.set("n", "<C-w>7", ":buffer 7<CR>", { desc = "Buffer 7" })
+    keymap.set("n", "<C-w>8", ":buffer 8<CR>", { desc = "Buffer 8" })
+    keymap.set("n", "<C-w>9", ":buffer 9<CR>", { desc = "Buffer 9" })
+
+    keymap.set("n", "<C-w>x", ":bdelete<CR>", { desc = "Delete buffer (tmux-like kill)" })
+    keymap.set("n", "<C-w>c", ":enew<CR>", { desc = "New empty buffer" })
 end
