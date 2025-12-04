898d3ea267b26bd43feedd609a6f78bc66a175de
NicoLuhmann <luhmann.nico@gmail.com>
2025-12-04 02:41:47 +0100

    feat: add tiny inline diagnostics+ros related lsps

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,48 +1,59 @@
 return {
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      -- list of servers for mason to install
-      ensure_installed = {
-        "bashls",
-        "clangd",
-        "lua_ls",
-        "pyright",
-      },
-    },
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-        opts = {
-          ui = {
-            icons = {
-              package_installed = "✓",
-              package_pending = "➜",
-              package_uninstalled = "✗",
-            },
-          },
-        },
-      },
-      "neovim/nvim-lspconfig",
-    },
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {
-      ensure_installed = {
-        "black",
-        "clang-format",
-        "gersemi",
-        "isort",
-        "prettierd",
-        "prettier",
-        "stylua",
-        "tree-sitter-cli",
-        "xmlformatter",
-      },
-    },
-    dependencies = {
-      "williamboman/mason.nvim",
-    },
-  },
+	{
+		"rachartier/tiny-inline-diagnostic.nvim",
+		event = "VeryLazy",
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			-- list of servers for mason to install
+			ensure_installed = {
+				"bashls",
+				"clangd",
+				"cmake",
+				"dockerls",
+				"jsonls",
+				"lemminx", -- xml
+				"lua_ls",
+				"pyright",
+				"yamlls",
+			},
+		},
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				opts = {
+					ui = {
+						icons = {
+							package_installed = "✓",
+							package_pending = "➜",
+							package_uninstalled = "✗",
+						},
+					},
+				},
+			},
+			{ "neovim/nvim-lspconfig" },
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = {
+				"black",
+				"clang-format",
+				"gersemi",
+				"isort",
+				"prettierd",
+				"prettier",
+				"stylua",
+				"tree-sitter-cli",
+				"xmlformatter",
+			},
+		},
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+	},
 }
