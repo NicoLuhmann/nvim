9f5dd7ccd426b641e03d859de1402fdbdcf85ec9
NicoLuhmann <luhmann.nico@gmail.com>
2025-12-19 23:45:04 +0100

    feat: switch to osc52 usage for clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -27,4 +27,15 @@ else
 	vim.o.backup = false
 	vim.o.writebackup = false
 	vim.o.swapfile = false
+	vim.g.clipboard = {
+		name = "OSC 52",
+		copy = {
+			["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+			["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+		},
+		paste = {
+			["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+			["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+		},
+	}
 end
