3585ad91ea653b76a974ce75e7efd1f3cbbc1a54
NicoLuhmann <luhmann.nico@gmail.com>
2026-01-12 23:55:00 +0100

    feat: add shfmt

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -14,6 +14,7 @@ return {
 			json = { "prettierd", "prettier", stop_after_first = true },
 			lua = { "stylua" },
 			python = { "isort", "black" },
+      sh = { "shfmt" },
 			xml = { "xmlformatter" },
 			yaml = { "prettierd", "prettier", stop_after_first = true },
 		},
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -47,6 +47,7 @@ return {
 				"isort",
 				"prettierd",
 				"prettier",
+        "shfmt",
 				"stylua",
 				"tree-sitter-cli",
 				"xmlformatter",
